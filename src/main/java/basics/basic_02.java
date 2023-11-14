86cc8537c5152b678a7d9f77a3b91717ccc43564
Mayank Singh <mayank190801@gmail.com>
2023-11-14 15:53:09 +0530

    Anonymous Variable

---
+++ src/main/java/basics/basic_02.java
@@ -0,0 +1,17 @@
+package basics;
+
+public class basic_02 {
+    public static void main(String[] args) {
+        System.out.println("hello there buddy!");
+
+
+        String s1 = "Hello there";
+        //reference variable
+        //s1 is an reference variable basically
+
+        //anonymous object ??
+        new String();
+        //You can not use these types of object once again, because you are not storing any reference anywhere!!!
+
+    }
+}
